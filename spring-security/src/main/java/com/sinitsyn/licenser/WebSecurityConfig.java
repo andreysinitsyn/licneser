5853b2e9177ddbdb8a0ea5df3ffad6ee121324b6
Andrey Sinitsyn <a_sinitsyn@yahoo.com>
2017-08-07 01:47:16 +0300

    custom filter

--- spring-security/src/main/java/com/sinitsyn/licenser/WebSecurityConfig.java
+++ spring-security/src/main/java/com/sinitsyn/licenser/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +25,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+
+        http.addFilterAfter(new CustomFilter(), BasicAuthenticationFilter.class);
     }
 
     @Autowired
